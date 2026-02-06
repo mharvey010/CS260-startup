21b9cd7a604be98e3028f8aa6cfe721c418c944d
mharvey010 <mveyhar1@gmail.com>
2026-02-06 16:49:53 -0700

    login page jsx completed

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 export function Login() {
   return (
     <main className="container-fluid text-center">
-      <div>login displayed here</div>
+      <h1>Ready to React?</h1>
+      <div>
+        <form method="get" action="play.html">
+
+          <div className="row g-3 align-items-center">
+            <div className="col-auto">
+              <label for="email" className="col-form-label">Email</label>
+            </div>
+            <div className="col-auto">
+              <input type="email" id="email" className="form-control" placeholder="name@example.com"/>
+            </div>
+          </div>
+
+          <div className="row g-3 align-items-center">
+            <div className="col-auto">
+              <label for="password" className="col-form-label">Password</label>
+            </div>
+            <div className="col-auto">
+              <input type="password" id="password" className="form-control" placeholder="password"/>
+            </div>
+          </div>
+
+          <button type="submit" id="login-btn" className="btn btn-primary btn-sm">Login</button>
+          <button type="submit" id="create-btn" className="btn btn-secondary btn-sm">Create</button>
+        </form>
+      </div>
     </main>
   );
 }
