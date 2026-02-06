e1b59c3593bcbe14f02d57759bb32728d2bc8ea6
mharvey010 <mveyhar1@gmail.com>
2026-02-06 16:02:02 -0700

    added more structure for app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,38 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login/login';
-import { Play } from './play/play';
-import { Scores } from './scores/scores';
+// import { Login } from './login/login';
+// import { Play } from './play/play';
+// import { Scores } from './scores/scores';
 
 export default function App() {
   return (
     <BrowserRouter>
-        
+        <div className="app bg-dark text-light">
+          <header>
+            <h1>Reactions</h1>
+            <nav>
+              <menu className="nav nav-underline">
+                <li className="nav-item"><a className="nav-link active" href="index.html">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="play.html">Play Reactions</a></li>
+                <li className="nav-item"><a className="nav-link" href="scores.html">Scoreboard</a></li>
+              </menu>
+            </nav>
+            <hr />
+          </header>
+
+          <main className="container-fluid text-center">
+            stuff to be filled in
+          </main>
+
+          <footer>
+            <div className="container-fluid">
+              <span>Author: Michael Harvey</span>
+              <a href="https://github.com/mharvey010/CS260-startup">GitHub</a>
+              <p>Discord Presence: (Third-party API integration placeholder)</p>
+            </div>
+          </footer>
+        </div>
     </BrowserRouter>
   );
 }
