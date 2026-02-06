f04cb60ef87d8d756a613c6c782fd1219c1cba7b
mharvey010 <mveyhar1@gmail.com>
2026-02-06 16:25:52 -0700

    nav links changed to work with browser router

--- src/app.jsx
+++ src/app.jsx
@@ -14,9 +14,9 @@ export default function App() {
             <h1>Reactions</h1>
             <nav>
               <menu className="nav nav-underline">
-                <li className="nav-item"><a className="nav-link active" href="index.html">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="play.html">Play Reactions</a></li>
-                <li className="nav-item"><a className="nav-link" href="scores.html">Scoreboard</a></li>
+                <li className="nav-item"><NavLink className="nav-link active" to="index">Home</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" to="play">Play Reactions</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" to="scores">Scoreboard</NavLink></li>
               </menu>
             </nav>
             <hr />
