f378ae924189ddf8008ce8af8909b9043fe9d489
mharvey010 <mveyhar1@gmail.com>
2026-02-06 23:08:08 -0700

    changed some html elements for play.jsx to match proper jsx syntax in order to render properly.

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './play.css';
 
 export function Play() {
   return (
     <main>
-      <div>play displayed here</div>
+        <div style={{ textAlign: 'right' }}>
+            Logged in as: <span>Username Placeholder</span>
+        </div>
+        <div style={{ textAlign: 'right' }}>
+            Personal Best Time: <span>00.00 seconds</span>
+        </div>
+        <div style={{ textAlign: 'center', marginTop: '50px', fontSize: '50px' }}>
+            0.000 seconds
+        </div>
+        <div id="reaction-button" style={{ textAlign: 'center' }}>
+            <button>
+                <img src="reaction_button.png" style={{ width: '300px', height: '300px' }}/>
+            </button>
+        </div>
+        <div id="game-start" style={{ textAlign: 'center', marginTop: '20px' }}>
+            <button style={{ fontSize: '30px' }}>Start Game</button>
+        </div>
     </main>
   );
 }
