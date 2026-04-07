b93025eef2ac83a13abab1bc7aa90ccb36004840
mharvey010 <mveyhar1@gmail.com>
2026-04-07 01:30:26 -0600

    functionality for getting the players personal best

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -3,15 +3,33 @@ import './play.css';
 import { ReactionGame } from './reactionGame';
 
 export function Play({ userName }) {
+  const [personalBest, setPersonalBest] = React.useState('00.00');
+
+  async function updatePersonalBest() {
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      const scores = JSON.parse(scoresText);
+      const userScores = scores.filter(score => score.name === userName);
+      if (userScores.length > 0) {
+        const bestScore = Math.min(...userScores.map(s => s.score));
+        setPersonalBest(bestScore);
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    updatePersonalBest();
+  }, [userName]);
+
   return (
     <main className="play-page">
         <div className="play-header" style={{ textAlign: 'right' }}>
             Logged in as: <span>{userName.split('@')[0]}</span>
         </div>
         <div className="player-info" style={{ textAlign: 'right' }}>
-            Personal Best Time: <span>00.00 seconds</span>
+            Personal Best Time: <span>{personalBest} seconds</span>
         </div>
-        <ReactionGame userName={userName} />
+        <ReactionGame userName={userName} onScoreSaved={updatePersonalBest} />
     </main>
   );
 }
