5b0a58ae1fd704531ce842d1400d2ad4e4db797f
mharvey010 <mveyhar1@gmail.com>
2026-04-07 03:18:15 -0600

    login or create user helper function added

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -17,6 +17,23 @@ export function Unauthenticated(props) {
     props.onLogin(userName);
   }
 
+  async function loginOrCreate(endpoint) {
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      props.onLogin(userName);
+    } else {
+      const body = await response.json();
+      setDisplayError(`⚠ Error: ${body.msg}`);
+    }
+  }
+
   return (
     <>
       <div>
