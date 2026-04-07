755adee5f21b63d2806f3ca81858798f1dba1ed5
mharvey010 <mveyhar1@gmail.com>
2026-04-07 09:16:39 -0600

    import database file

--- service/index.js
+++ service/index.js
@@ -3,11 +3,9 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
-
-let users = [];
-let scores = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
