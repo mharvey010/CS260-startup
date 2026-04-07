725a3c3bc0047a109eb6ac7076588220b815a768
mharvey010 <mveyhar1@gmail.com>
2026-04-07 03:05:51 -0600

    importing all the necessary modules like bcrypt, express, cookie-parser

---
+++ service/index.js
@@ -0,0 +1,21 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
+let scores = [];
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
