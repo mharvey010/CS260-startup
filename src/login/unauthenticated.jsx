7916925d19582a9d9b1a16cf077d67fa623c9c35
mharvey010 <mveyhar1@gmail.com>
2026-04-06 20:47:26 -0600

    added unauthenticated jsx page for standard login screen

---
+++ src/login/unauthenticated.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { MessageDialog } from './messageDialog';
+
+export function Unauthenticated(props) {
+  const [userName, setUserName] = React.useState(props.userName);
+  const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
+
+  async function loginUser() {
+    localStorage.setItem('userName', userName);
+    props.onLogin(userName);
+  }
+
+  async function createUser() {
+    localStorage.setItem('userName', userName);
+    props.onLogin(userName);
+  }
+
+  return (
+    <>
+      <div>
+        <div className="row g-3 align-items-center">
+          <div className="col-auto">
+            <label htmlFor="email" className="col-form-label">Email</label>
+          </div>
+          <div className="col-auto">
+            <input type="email" id="email" className="form-control" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="name@example.com"/>
+          </div>
+        </div>
+
+        <div className="row g-3 align-items-center">
+          <div className="col-auto">
+            <label htmlFor="password" className="col-form-label">Password</label>
+          </div>
+          <div className="col-auto">
+            <input type="password" id="password" className="form-control" onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
+          </div>
+        </div>
+
+        <button type="button" id="login-btn" className="btn btn-primary btn-sm" onClick={() => loginUser()} disabled={!userName || !password}>Login</button>
+        <button type="button" id="create-btn" className="btn btn-secondary btn-sm" onClick={() => createUser()} disabled={!userName || !password}>Create</button>
+      </div>
+
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+    </>
+  );
+}
