7c509bc0780e5d87b9d0fbc0461b902563a1587d
mharvey010 <mveyhar1@gmail.com>
2026-04-06 21:06:54 -0600

    set up general login page jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="container-fluid text-center">
-      <h1>Ready to React?</h1>
       <div>
-        <form method="get" action="play.html">
-
-          <div className="row g-3 align-items-center">
-            <div className="col-auto">
-              <label for="email" className="col-form-label">Email</label>
-            </div>
-            <div className="col-auto">
-              <input type="email" id="email" className="form-control" placeholder="name@example.com"/>
-            </div>
-          </div>
-
-          <div className="row g-3 align-items-center">
-            <div className="col-auto">
-              <label for="password" className="col-form-label">Password</label>
-            </div>
-            <div className="col-auto">
-              <input type="password" id="password" className="form-control" placeholder="password"/>
-            </div>
-          </div>
-
-          <button type="submit" id="login-btn" className="btn btn-primary btn-sm">Login</button>
-          <button type="submit" id="create-btn" className="btn btn-secondary btn-sm">Create</button>
-        </form>
+        {authState !== AuthState.Unknown && <h1>Ready to React?</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
       </div>
     </main>
   );
